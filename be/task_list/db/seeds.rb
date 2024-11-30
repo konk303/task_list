ef5fb30af291a5b2e2ad954fc9e73a3591a9edee
Kimihiko NAKAMURA <nakamura.808@gmail.com>
2024-11-30 13:35:22 +0900

    Create seed data

--- be/task_list/db/seeds.rb
+++ be/task_list/db/seeds.rb
@@ -7,3 +7,14 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+List.find_or_create_by!(name: '冬の旅行') do |list|
+  %w[行先決定 日程決定 休暇取得 チケット購入 宿予約].each_with_index do |name, i|
+    list.tasks.build(name: name, order: i)
+  end
+end
+
+List.find_or_create_by!(name: '春の旅行') do |list|
+  %w[行先決定 日程決定 休暇取得 チケット購入 宿予約].each_with_index do |name, i|
+    list.tasks.build(name: name, order: i, done: i.odd?)
+  end
+end
