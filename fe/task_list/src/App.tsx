0166f3ce395adbe1d494a2861e851b2869559476
Kimihiko NAKAMURA <nakamura.808@gmail.com>
2024-12-01 13:11:40 +0900

    Setup apollo client

--- fe/task_list/src/App.tsx
+++ fe/task_list/src/App.tsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useQuery, gql } from '@apollo/client';
 
-function App() {
-  const [count, setCount] = useState(0)
+const GET_LOCATIONS = gql`
+  { lists
+    { id name }
+  }
+`;
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+function DisplayLocations() {
+  const { loading, error, data } = useQuery(GET_LOCATIONS);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error : {error.message}</p>;
+
+  return data.lists.map(({ id, name }) => (
+    <div key={id}>
+      <h3>{name}</h3>
+      <br />
+    </div>
+  ));
 }
 
-export default App
+
+
+
+
+export default function App() {
+  return (
+    <div>
+      <h2>My first Apollo app 🚀</h2>
+      <br />
+      <DisplayLocations />
+    </div>
+  );
+}
