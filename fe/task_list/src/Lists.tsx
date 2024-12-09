b778075bdd352f92c285643ab2848c87949354b1
Kimihiko NAKAMURA <nakamura.808@gmail.com>
2024-12-09 14:19:35 +0900

    Style tweaks

--- fe/task_list/src/Lists.tsx
+++ fe/task_list/src/Lists.tsx
@@ -15,7 +15,7 @@ export default function Lists() {
   if (error) return <p>Error : {error.message}</p>;
 
   const lists: [ListType] = data.lists
-  const options = lists.map(({ id, name }, i: number) => (<option key={i} value={i}>{name}({id})</option>))
+  const options = lists.map(({ id, name }, i: number) => (<option key={i} value={i}>{name}</option>))
   return (
     <div>
       <select value={listIndex} onChange={e => setListIndex(+ e.target.value)}>{options}</select>
