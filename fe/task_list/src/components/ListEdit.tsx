21a36b3745c96c1b01d25e4a0f0b9a849f579f1b
Kimihiko NAKAMURA <nakamura.808@gmail.com>
2024-12-23 00:00:21 +0900

    Confirm Dialog

--- fe/task_list/src/components/ListEdit.tsx
+++ fe/task_list/src/components/ListEdit.tsx
@@ -3,6 +3,7 @@ import { Button } from './ui/button.tsx';
 import { PopoverArrow, PopoverBody, PopoverContent, PopoverRoot, PopoverTitle } from './ui/popover.tsx';
 import { List } from '../__generated__/graphql.ts';
 import { ChangeEvent } from 'react';
+import ListEditDialog from './ListEditDialog.tsx';
 
 export default function ListEdit ({
     name,
@@ -42,13 +43,19 @@ export default function ListEdit ({
                                     onChange={ changeNameHandler }
                                     size="2xl"
                                 />
-                                <Button
-                                    size="sm"
+                                <ListEditDialog
+                                    text="リストを削除しますか？"
                                     colorPalette="red"
-                                    onClick={ deleteListHandler }
+                                    label="削除"
+                                    executeHandler={ deleteListHandler }
                                 >
-                                    <span className={ "material-icons" }>delete</span>
-                                </Button>
+                                    <Button
+                                        size="sm"
+                                        colorPalette="red"
+                                    >
+                                        <span className={ "material-icons" }>delete</span>
+                                    </Button>
+                                </ListEditDialog>
                             </Flex>
                         </Tabs.Content>
                         <Tabs.Content value="tab-2">
@@ -58,13 +65,19 @@ export default function ListEdit ({
                                     value={ newName || "" }
                                     onChange={ changeNewNameHandler }
                                     placeholder="新規登録" size="2xl" />
-                                <Button
-                                    size="sm"
+                                <ListEditDialog
+                                    text="リストを追加しますか？"
                                     colorPalette="blue"
-                                    onClick={ createListHandler }
+                                    label="追加"
+                                    executeHandler={ createListHandler }
                                 >
-                                    <span className={ "material-icons" }>add</span>
-                                </Button>
+                                    <Button
+                                        size="sm"
+                                        colorPalette="blue"
+                                    >
+                                        <span className={ "material-icons" }>add</span>
+                                    </Button>
+                                </ListEditDialog>
                             </Flex>
                         </Tabs.Content>
                     </Tabs.Root>
---
+++ fe/task_list/src/components/ListEditDialog.tsx
@@ -0,0 +1,47 @@
+import { ReactNode } from 'react';
+import { DialogActionTrigger, DialogBody, DialogCloseTrigger, DialogContent, DialogFooter, DialogHeader, DialogRoot, DialogTitle, DialogTrigger } from './ui/dialog.tsx';
+import { Button } from './ui/button.tsx';
+
+export default function ListEditDialog ({
+    children,
+    text,
+    colorPalette,
+    label,
+    executeHandler
+}: {
+    children: ReactNode
+    text: string
+    colorPalette: string
+    label: string
+    executeHandler: () => void
+}) {
+    return (
+        <DialogRoot
+            role="alertdialog"
+            placement="center"
+        >
+            <DialogTrigger asChild>
+                { children }
+            </DialogTrigger>
+            <DialogContent>
+                <DialogHeader>
+                    <DialogTitle>Are you sure?</DialogTitle>
+                </DialogHeader>
+                <DialogBody>
+                    <p>{ text }</p>
+                </DialogBody>
+                <DialogFooter>
+                    <DialogActionTrigger asChild>
+                        <Button variant="outline">キャンセル</Button>
+                    </DialogActionTrigger>
+                    <Button
+                        colorPalette={ colorPalette }
+                        onClick={ executeHandler }
+                    >{ label }</Button>
+                </DialogFooter>
+                <DialogCloseTrigger />
+            </DialogContent>
+        </DialogRoot>
+
+    )
+}
